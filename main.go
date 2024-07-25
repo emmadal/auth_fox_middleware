4b9091d16311bd25bfc3796b743359c3b6c7bc14
emmadal <emmanueldalougou@gmail.com>
2024-07-25 01:22:06 +0000

    feat: add authorization middleware

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var context *gin.Context
+
+func main() {
+
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "Unauthorized",
+		})
+		return
+	}
+
+	//  validate the token and get user data from it
+	userID, err := verifyToken(token)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "Unauthorized",
+		})
+		return
+	}
+
+	// Attach the userID to the request
+	context.Set("userID", userID)
+
+	context.Next()
+
+}
---
+++ verify_token.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// verifyToken verify the given token to get its payload.
+func verifyToken(tokenString string) (int64, error) {
+	// verify the signature of the token
+	parsedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Invalid signature")
+		}
+		return []byte(os.Getenv("SECRETKEY")), nil
+	})
+
+	if err != nil {
+		return 0, errors.New("Couldn't handle this token")
+	}
+
+	// check the validity of token
+	if tokenIsValid := parsedToken.Valid; !tokenIsValid {
+		return 0, errors.New("Invalid token")
+	}
+
+	// extract claims data in the token
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("Invalid token claims")
+	}
+	userID := int64(claims["userID"].(float64)) // .(float64) means type checking
+
+	return userID, nil
+}
